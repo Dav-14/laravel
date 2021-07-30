1f1fb7a7768a352391a17983620bd2881e738d19
Dav-14 <david.ragot14@gmail.com>
2021-07-30 23:03:01 +0200

    More routee, Add multiple Requests Methods

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
     public function get(){
-        return view('infos');
+        return view('contact');
     }
 
 
-    public function post(Request $request){
-        return 'le nom est ' . $request->input('nom');
+    public function post(ContactRequest $request){
+
+        Mail::send('emails.contact', $request->all(), function($message)
+        {
+            $message
+            ->to('david.ragot14@orange.fr')
+            ->subject('Contact')
+            ->from('david.ragot14@gmail.com');//Devrais par defaut, dans .env MAIL_USERNAME
+        });
+
+        return view('confirm');
     }
 }
